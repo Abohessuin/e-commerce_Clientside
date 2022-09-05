886ddc8d1e1fa1ed666f2700871507d0638b3b6b
abohessuin <eng.mohamedhessuin@gmail.com>
2022-09-05 12:32:53 +0200

    fix bug in counter

--- src/components/search_filters/components/SearchFilters.js
+++ src/components/search_filters/components/SearchFilters.js
@@ -98,6 +98,7 @@ const SearchFilters = () => {
             filtername={"Price"}
             tempFilters={tempFilters}
             setTempFilters={setTempFilters}
+            filterkey={"price"}
           >
             <SingleGrapInputView
               filterkey={"price"}
